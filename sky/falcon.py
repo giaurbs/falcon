aa68d72eb1e5bfc400f325ffd637b1fd24d413a6
giaurbs <giaurbs@gmail.com>
2016-02-03 16:02:14 +0000

    Update falcon.py

--- sky/falcon.py
+++ sky/falcon.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-
+from tabulate import tabulate
 from matrix import build_matrix
-from matrix import sort_matrix
 
-matrix = sort_matrix(build_matrix()[1])
+#from matrix import sort_matrix
+#matrix = sort_matrix(build_matrix()[1])
+matrix = build_matrix()[1]
 
-print(matrix[:20])
+print(tabulate(matrix))
+#print tabulate(matrix)
